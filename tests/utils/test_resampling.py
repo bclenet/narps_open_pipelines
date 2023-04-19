5c55e8bb98ccb84a05c4d8341e84eb4044afe455
Boris Clénet <boris.clenet@inria.fr>
2023-04-19 10:20:10 +0200

    [TEST] tests for the resampling module

---
+++ tests/utils/test_resampling.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# coding: utf-8
+
+""" Tests of the 'narps_open.utils.resampling' module.
+
+Launch this test with PyTest
+
+Usage:
+======
+    pytest -q test_resampling.py
+    pytest -q test_resampling.py -k <selected_test>
+"""
+
+from os.path import join
+from filecmp import cmp
+
+from pytest import mark
+
+from narps_open.utils.configuration import Configuration
+from narps_open.utils.resampling import resample_image
+
+class TestResampling:
+    """ A class that contains all the unit tests for the resampling module."""
+
+    @staticmethod
+    @mark.unit_test
+    def test_resample():
+        """ Test the resample_image function """
+        input_file = join(
+            Configuration()['directories']['test_data'],
+            'utils','resampling',
+            'input_file.nii.gz'
+            )
+        output_file_4 = join(
+            Configuration()['directories']['test_runs'],
+            'utils','resampling',
+            'test_resample_4.nii.gz'
+            )
+        reference_file_4 = join(
+            Configuration()['directories']['test_data'],
+            'utils','resampling',
+            'reference_file_4.nii.gz'
+            )
+        output_file_05 = join(
+            Configuration()['directories']['test_runs'],
+            'utils','resampling',
+            'test_resample_05.nii.gz'
+            )
+        reference_file_05 = join(
+            Configuration()['directories']['test_data'],
+            'utils','resampling',
+            'reference_file_05.nii.gz'
+            )
+
+        resample_image(input_file, output_file_4, 4)
+        assert cmp(output_file_4, reference_file_4)
+
+        resample_image(input_file, output_file_05, .5)
+        assert cmp(output_file_05, reference_file_05)
